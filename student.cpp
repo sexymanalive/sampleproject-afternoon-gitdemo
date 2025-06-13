0b62a3a5583bd8bb0a691a7db9b8ff700735f74a
sexymanalive <lyvanna544@gmail.com>
2025-06-13 09:20:10 +0000

    fix: adding student class

--- student.cpp
+++ student.cpp
@@ -1,6 +1,11 @@
 #include<iostream> 
 using namespace std; 
 
+class Student{
+    public: 
+        string name; 
+        int age; 
+}; 
 int main(){
     cout<<"Student Demo file "<<endl; 
     return 0 ; 
